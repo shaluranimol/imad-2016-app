d828fe4065952bb9d21e4b5e76cce24985134346
shaluranimol <shalet@gmail.com>
2016-10-14 18:21:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-    res.send("Article two is served here");
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three' ,function(req,res){
     res.send("Article three is served here");
